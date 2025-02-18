6a314e6f60f42873e0551403f04c6c03991830b9
Sarthak <nautiyal.sarthak@gmail.com>
2025-02-18 14:52:40 -0500

    dev

--- app.py
+++ app.py
@@ -226,4 +226,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT as an environment variable
+    app.run(host="0.0.0.0", port=port)
